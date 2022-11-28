cce6a42b2416429be20d91fc966a0b5dcc497aea
Abdulaziz-rgb <shokirovabdulaziz19@gmail.com>
2022-11-28 11:56:55 +0500

    initial RestDemo project

---
+++ Catalog/Entities/DatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+namespace Catalog.Entities
+{
+    public class DatabaseSettings
+    {
+        public string? ConnectionString { get; set; }
+
+        public string? DatabaseName { get; set; }
+
+        public string? BooksCollectionName { get; set; }
+    }
+}
